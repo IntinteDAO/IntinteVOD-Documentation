2a30de7840c851728e658aaeb82b773e5e5c9beb
fervi <lordfervi@protonmail.com>
2019-11-06 18:20:06 +0100

    Removed "subtitles" array from JSON, because it is no longer necessary.

--- json_generator_template.php
+++ json_generator_template.php
@@ -10,7 +10,6 @@ $movie["rating"]=0;
 $movie["genre"][0]=0;
 $movie["genre"][1]=0;
 $movie["genre"][2]=0;
-$movie["subtitles"]="";
 $movie["enchanted"]=0;
 $movie["next_episode"]="";
 $movie["previous_episode"]="";
